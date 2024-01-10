79af0638580d6a718f79c5a5e10a10e49552d121
azziimm7 <azzah31@outlook.com>
2024-01-10 07:37:45 +0000

    progressing:

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,14 @@
+s module contains a function that convert a JSON to python object"""
+
+
+from json import loads
+
+
+def from_json_string(my_str):
+    """Serialize a JSON to python object
+
+    Args:
+        my_str (str): The string we want to convert to python object
+    """
+
+    return (loads(my_str))
---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+s module contains a script that Deserialize
+a JSON file, append to its content, and save the result"""
+
+
+from json import load
+from sys import argv
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+my_args = argv[1:]
+try:
+    my_list = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    my_list = []
+
+for arg in my_args:
+    my_list.append(arg)
+
+save_to_json_file(my_list, "add_item.json")
