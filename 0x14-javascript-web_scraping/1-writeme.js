15c84b28baa8b85cef999bdc3b53a292e399d283
azziimm7 <azzah31@outlook.com>
2024-05-21 05:36:01 -0700

    task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 
-const fs = require("fs");
-const file = process.argv[2]
-const data = process.argv[3]
+const fs = require('fs');
+const file = process.argv[2];
+const data = process.argv[3];
 
 // using fs.writeFile method
 fs.writeFile(file, data, 'utf-8', (err) => {
-  if (err)
+  if (err) {
     console.log(err);
+  }
 });
