15dbeed2a691294f783545d15068feb7fac42420
azziimm7 <azzah31@outlook.com>
2024-05-21 05:05:47 -0700

    semistandard task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-//inlude fs module
+// inlude fs module
 const fs = require('fs');
 const filename = process.argv[2];
 
 // use fs.readfile to read content
-fs.readFile(filename, 'utf-8', function(err, data){
-	// Display file content or err
-	console.log(err || data);
+fs.readFile(filename, 'utf-8', function (err, data) {
+  // Display file content or err
+  console.log(err || data);
 });
