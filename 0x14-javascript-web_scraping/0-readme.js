a5c644fa94962169c56cbba4927c1deddde6424f
azziimm7 <azzah31@outlook.com>
2024-05-21 04:55:32 -0700

    task 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+//inlude fs module
+const fs = require('fs');
+const filename = process.argv[2];
+
+// use fs.readfile to read content
+fs.readFile(filename, 'utf-8', function(err, data){
+	// Display file content or err
+	console.log(err || data);
+});
